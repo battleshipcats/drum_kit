fdc7100f17a830bbcd5b5c20f0ebc32a0b8431ad
Lisa Szymanski <43975456+battleshipcats@users.noreply.github.com>
2024-01-08 23:41:11 -0500

    Drum machine
    git commit -m <This is a drum machine website that I am adding to GitHub for the first time>

---
+++ index.js
@@ -0,0 +1,79 @@
+
+/* document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+    alert("I got clicked!")
+}
+ */
+/* document.querySelector("button").addEventListener("click", function () {
+    alert("I got clicked!")
+}); */
+
+//This works, but is not Angela's answer.
+/* document.body.addEventListener("click", event => {
+    if (event.target.nodeName == "BUTTON") {
+        alert("I got clicked!");
+    }
+  }); */
+
+
+//detect button press
+var allButtons = document.querySelectorAll(".drum").length;
+
+for (var i=0; i<allButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        makeSound(this.innerHTML);
+    });
+  }
+//detect key press
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+  });
+
+function makeSound(key) {
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+    
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+            
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        default: console.log(key);
+            break;
+    }
+};
+
+
+          //alert("I clicked the "+(this.innerHTML)+" key!");
+  
+
